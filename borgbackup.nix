adedd089a8016d5ded04b32f90cd1aaa5ca12fe7
Marshall McMullen <marshall.mcmullen@gmail.com>
2022-08-20 07:27:25 -0600

    Add borgbackup.nix

---
+++ borgbackup.nix
@@ -0,0 +1,53 @@
+{ config, ... }:
+
+{
+  services.borgbackup.jobs."system" = {
+    doInit = true;
+    repo = "ssh://borg-backup@lazarus/volume1/BorgBackup/${config.networking.hostName}";
+    encryption.mode = "none";
+    environment.BORG_RSH = "ssh -i /usr/local/etc/ssh/borg -o BatchMode=yes -o VerifyHostKeyDNS=yes";
+    compression = "auto,lz4";
+    startAt = "daily";
+ 
+    paths = [
+      "/etc/nixos"
+    ];
+
+    exclude = [
+      # Linux System Files
+      "/dev"
+      "/mnt"
+      "/proc"
+      "/run"
+      "/sys"
+      "/tmp"
+      "/var/cache"
+      "/var/run"
+      "/var/tmp"
+
+      # Nix
+      "/nix"
+
+      # very large files
+      "/var/lib/docker"
+      "/var/lib/kubelet"
+      "/var/lib/systemd"
+      "/var/lib/libvirt"
+   
+      # Temporary and cache files
+      "**/.cache"
+      "**/Cache"
+      "/home/**/go/bin"
+      "/home/**/go/pkg"
+    ];
+  
+    prune.keep = {
+      daily = 7;
+      weekly = 4;
+      monthly = 6;
+    }; 
+
+    extraCreateArgs = "--verbose --stats";
+
+  };
+}
