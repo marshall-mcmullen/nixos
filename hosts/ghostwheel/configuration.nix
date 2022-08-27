aaaa1e282407e46628d70d837e72ee5aec67fa70
Marshall McMullen <marshall.mcmullen@gmail.com>
2022-08-26 21:45:21 -0600

    Add ghostwheel configs.

---
+++ hosts/ghostwheel/configuration.nix
@@ -0,0 +1,45 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+
+    # Host specific
+    ./hardware-configuration.nix
+    ./networking.nix
+    ./packages.nix
+
+    # Zentire modules
+    ./modules/bitwarden.nix
+    ./modules/boot-zfs.nix
+    ./modules/borgbackup.nix
+    ./modules/docker.nix
+    ./modules/localization.nix
+    ./modules/mail.nix
+    ./modules/networking.nix
+    ./modules/nginx.nix
+    ./modules/packages.nix
+    ./modules/pihole.nix
+    ./modules/plex.nix
+    ./modules/raid.nix
+    ./modules/security.nix
+    ./modules/services.nix
+    ./modules/systemd-email.nix
+    ./modules/upgrade-check.nix
+    ./modules/users.nix
+    ./modules/wordpress.nix
+  ];
+
+  #===============#
+  # DO NOT MODIFY #
+  #===============#
+
+  # Optimize Nix storage
+  nix.autoOptimiseStore = true;
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix).
+  system.copySystemConfiguration = true;
+
+  # Set this to the current release version
+  system.stateVersion = "22.05"; # Did you read the comment?
+}
